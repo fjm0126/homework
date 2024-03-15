5da6ce905323ea6b2f88be8bffc157195fc0ad53
fjm0126 <2021533129@qq.com>
2024-03-15 22:14:35 +0800

    first

---
+++ Small/Week_1/jdbc/src/User/main.java
@@ -0,0 +1,12 @@
+package User;
+
+import java.sql.SQLException;
+
+public class main {
+    public static void main(String[] args) throws SQLException {
+        ATM atm=new ATM();
+        atm.Menu();
+    }
+}
+
+
